682612b24b10d4c85c317a2f9a6a6c1f52c209b1
yusuf <mail@yusufsenel.de>
2022-10-10 16:27:18 +0200

    1

--- private/class/categories.class.php
+++ private/class/categories.class.php
@@ -4,7 +4,7 @@ class categories{
 
     public function clothes(): array
     {
-        $categorie = file_get_contents("/home/yusuf/PhpstormProjects/shopix/private/daten/categorie.json");
+        $categorie = file_get_contents(__DIR__ . "/../daten/categorie.json");
         $decodedText = html_entity_decode($categorie);
         try {
             $produktjs = json_decode($decodedText, true, 512, JSON_THROW_ON_ERROR);
