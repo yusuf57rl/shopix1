98e26f5ce51aefeed681ca0cb376ea0666b2ba60
Yusuf S <mail@yusufsenel.de>
2022-10-12 11:58:13 +0200

    rr

--- private/class/products.php
+++ private/class/products.php
@@ -14,9 +14,12 @@ class products
         return json_decode($decodedText2, true, 512, JSON_THROW_ON_ERROR);
     }
 
-    public function getProductsByCategoryId(int $categoryId): array
+    public function getProductsByCategoryId(int $categoryId, $decodedText2): array
     {
         $allProducts = $this->getAllProducts();
+        if($categoryId === $decodedText2["categoryid"]){
+            return($decodedText2);
+        }
         // filter nach den richtigen Produkten aus der Kategorie und zurückgeben
     }
 
